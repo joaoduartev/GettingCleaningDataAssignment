1d72c5e5cdade2211a24e9b6f14a51827a4a3f4b
joaoduartev <joaovpduarte@gmail.com>
2020-09-09 10:26:58 -0300

    Saving files

---
+++ Assignment.R
@@ -0,0 +1,28 @@
+library(dplyr)
+
+setwd('./Getting and Cleaning Data/UCI HAR Dataset')
+nomes <- read.table('features.txt')[,2]
+
+setwd('./test')
+test <- read.table('X_test.txt')
+colnames(test) <- nomes
+test <- test[grep('.mean.|.std.', nomes, ignore.case = TRUE, value = TRUE)]
+test <- cbind(read.table('y_test.txt'), test)
+
+setwd('../')
+setwd('./train')
+train <- read.table('X_train.txt')
+colnames(train) <- nomes
+train <- train[grep('.mean.|.std.', nomes, ignore.case = TRUE, value = TRUE)]
+train <- cbind(read.table('y_train.txt'), train)
+
+dados <- rbind(test, train)
+setwd('../')
+activity <- read.table('activity_labels.txt')
+
+dados[1] <- left_join(dados, activity)[ncol(left_join(dados, activity))]
+names(dados)[1] = 'activity'
+
+resumo <- dados %>% group_by(activity) %>% summarise_all(mean)
+
+write.table(resumo, file = "tidy_data.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
